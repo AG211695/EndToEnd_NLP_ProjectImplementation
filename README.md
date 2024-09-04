b424cd0b5e2b5bc1bf31f8440878389121dd4a01
AG211695 <suzfall20@gmail.com>
2024-09-04 13:45:28 -0400

    done till data tranforamtion

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # EndToEnd_NLP_ProjectImplementation
+
+#Command to create env
+conda create -n nlpHsr python=3.8 -y
+
+#Command to activate the env
+conda activate nlphsr
+
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hateSpeechclassification",
+    version="0.0.1",
+    author="SusanDS",
+    author_email="susanan1621@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
